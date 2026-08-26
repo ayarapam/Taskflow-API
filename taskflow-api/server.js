f05bc84548e50b31bc26fc04719b794cea7a2684
ayara <ayara.dantas@aluno.senai.br>
2026-08-26 11:16:24 -0300

    Semana 9 · Dia 3

--- taskflow-api/server.js
+++ taskflow-api/server.js
@@ -174,7 +174,6 @@ app.get('/tarefas', (req, res) => {
 // req.query é um parametro de pergunta, 
 // req.query = uma desconstrução,
 // ela checa uma informação dentro do Json e apartir dela filtra
-
 //--------------------------------------------
 // 4 . Rota 404 — capturar rotas não encontradas
 //--------------------------------------------
